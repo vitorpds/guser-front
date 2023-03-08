4f678428c63c0262c96cb650725784e974c0ac0a
vitorpds <souza_vitor14@hotmail.com>
2023-03-08 09:48:58 -0300

    correcoes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,6 +20,8 @@ class UserController {
             btn.disabled = true;
             
             let values = this.getValues();
+            
+            if (!values) return false;
 
             this.getPhoto().then((content) => {
 
@@ -42,7 +44,16 @@ class UserController {
     getValues() {
         let user = {};
 
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function(field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             if (field.name == 'gender') {
 
@@ -62,6 +73,10 @@ class UserController {
 
             }
         });
+
+        if (!isValid) {
+            return false;
+        }
     
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
     }
@@ -113,6 +128,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -126,5 +143,29 @@ class UserController {
         `;
         
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            console.log(user);
+
+            if (user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
     }
 }
